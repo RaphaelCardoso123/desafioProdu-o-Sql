ca0544c24b5edb43756fd0a994671ccefa3a890f
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2023-01-04 19:42:42 -0300

    ...

--- desafioSql.sql
+++ desafioSql.sql
@@ -1,6 +1,7 @@
 use desafio;
 create schema producao;
 
+
 create table producao.linha_producao(
 	cd_linha_producao int primary key identity(1, 1),
 	dt_linha_producao date not null
@@ -16,6 +17,7 @@ create table producao.produto(
 	dt_produto date not null,
 	cd_linha_producao int not null,
 	cd_tipo_produto int not null,
+	nm_tipo_produto varchar(30) not null,
 	foreign key(cd_linha_producao) references producao.linha_producao(cd_linha_producao),
 	foreign key(cd_tipo_produto) references producao.tipo_produto(cd_tipo_produto)
 );
@@ -25,26 +27,38 @@ create table producao.avaliacao(
 	ds_avaliacao varchar(30) not null
 );
 
+--não pode ser identity(1, 1) auto incrementável pq é um código único para cada inspetor, 
+--mas de outra forma não se pode repetir o código por ser primary key
+--como fazer???
 create table producao.inspetor(
 	cd_matricula_inspetor int primary key identity(1, 1),
-	nm_inspetor varchar(30) not null
+	nm_inspetor varchar(30) not null,
+	dt_trabalho date not null,
+	hr_inicio_trabalho time not null,
+	hr_fim_trabalho time not null,
 );
 
 create table producao.ficha(
 	cd_numero_ficha int primary key identity(1, 1),
 	dt_inspecao date not null,
 	cd_matricula_inspetor int not null,
+	nm_inspetor varchar(30) not null,
 	foreign key(cd_matricula_inspetor) references producao.inspetor(cd_matricula_inspetor)
 );
 
 --talvez não precise fazer essa tabela tão cheia pq no fim vou usar comandos que vai unir tabelas diferentes
 create table producao.controle_qualidade( 
 	cd_controle_qualidade int primary key identity(1, 1),
-	hr_controle_qualidade time not null,
+	dt_controle_qualidade date not null,
+	hr_inicio_controle_qualidade time not null,
+	hr_fim_controle_qualidade time not null,
 	cd_numero_ficha int not null,--foreign key
 	dt_inspecao date not null,
-	nm_inspetor varchar(30) not null,
 	cd_matricula_inspetor int not null,--foreign key
+	nm_inspetor varchar(30) not null,
+	dt_trabalho date not null,
+	hr_inicio_trabalho time not null,
+	hr_fim_trabalho time not null,
 	cd_id_produto int not null,--foreign key
 	cd_linha_producao int not null,--foreign key
 	dt_linha_producao date not null,
@@ -58,9 +72,11 @@ create table producao.controle_qualidade(
 	foreign key(sg_avaliacao) references producao.avaliacao(sg_avaliacao)
 );
 
+
 begin transaction;
 insert into producao.linha_producao(dt_linha_producao)
-values('20221201'), ('20221205'), ('20221216'), ('20221218'), ('20221222')
+values('20221201'), ('20221205'), ('20221207'), ('20221209'), ('20221210'), 
+	  ('20221214'), ('20221216'), ('20221218'), ('20221220'), ('20221222')
 commit;
 select * from producao.linha_producao;
 
@@ -79,51 +95,21 @@ values('OK', 'Liberado'), ('EL', 'Problema elétrico'), ('PT', 'Problema de pintu
 commit;
 select * from producao.avaliacao;
 
-
 begin transaction;
-insert into producao.inspetor(nm_inspetor)
-values('Trancoso da Silva'), ('Pedro do Monte'), ('José Carmelo'), ('Júlio Cardoso'), ('Carlos Merinda')
+insert into producao.inspetor(nm_inspetor, dt_trabalho, hr_inicio_trabalho, hr_fim_trabalho)
+values('Trancoso da Silva', '20221201', '9:05', '11:05'), ('Trancoso da Silva', '20221205', '9:00', '11:00'),
+	  ('Trancoso da Silva', '20221207', '9:10', '11:10'), ('Trancoso da Silva', '20221216', '9:00', '11:10'),
+	  ('Pedro do Monte', '20221209', '9:00', '11:00'), ('Pedro do Monte', '20221210', '9:10', '11:10'),
+	  ('José Carmelo', '20221214', '9:05', '11:05'), ('José Carmelo', '20221218', '9:15', '11:15'),
+	  ('Júlio Cardoso', '20221220', '9:00', '11:15'), ('Júlio Cardoso', '20221222', '9:00', '11:00')
 commit;
 select * from producao.inspetor;
 
-
-begin transaction; --ficaria legal adicionar por último o nm_inspetor
-insert into producao.ficha(dt_inspecao, cd_matricula_inspetor)
+--ainda não está feito, só foi copiado e colado do antigo
+begin transaction; 
+insert into producao.ficha(dt_inspecao, cd_matricula_inspetor, nm_inspetor)
 values('20221201', 3), ('20221202', 2), ('20221206', 3), ('20221207', 4), ('20221210', 5),
 ('20221215', 1), ('20221216', 2), ('20221217', 3), ('20221218', 4), ('20221219', 5),
 ('20221220', 3), ('20221221', 1), ('20221222', 5), ('20221223', 4), ('20221224', 5)
 commit;
 select * from producao.ficha;
-
-
-begin transaction;--ficaria legal adicionar por último nm_produto
-insert into producao.produto(dt_produto, cd_linha_producao, cd_tipo_produto)
-values('20221201', 1, 2), ('20221209', 2, 3), ('20221216', 1, 1), ('20221218', 4, 5), ('20221222', 5, 4)
-commit;
-select * from producao.produto;
-
-
-begin transaction; --ficaria legal colocar datas diferentes em dt_inspecao / dt_linha_producao
-insert into producao.controle_qualidade(cd_numero_ficha, dt_inspecao, nm_inspetor, cd_matricula_inspetor, cd_id_produto,
-cd_linha_producao, dt_linha_producao, cd_tipo_produto, sg_avaliacao, hr_controle_qualidade)
-values(15, '20221216', 'Trancoso da Silva', 1, 5, 1, '20221201', 4, 'EL', '9:05:15'), 
-	  (15, '20221216', 'Trancoso da Silva', 1, 4, 4, '20221201', 1, 'OK', '9:20:10'),
-	  (15, '20221216', 'Trancoso da Silva', 1, 2, 3, '20221201', 2, 'PT', '9:35:26'),
-	  (15, '20221216', 'Trancoso da Silva', 1, 5, 3, '20221201', 3, 'OK', '9:50:40'), 
-	  (15, '20221216', 'Trancoso da Silva', 1, 1, 1, '20221201', 4, 'OK', '10:00:00'),
-	  (15, '20221216', 'Trancoso da Silva', 1, 3, 1, '20221201', 4, 'OK', '10:15:29')
-commit;
-select * from producao.controle_qualidade;
-
--- hr_inicio_controle_qualidade / hr_fim_controle_qualidade (para cada dia) -> Para responder pergunta 1.
--- hr_inicio_trabalho / hr_fim_trabalho (para cada dia) -> Para responder pergunta 2.
--- sg_avaliacao (já tem) / precisa ter dt_inspecao diferentes (está tudo no msm dia) -> Para responder pergunta 3.
--- preciso ter outros inspetores na ficha avaliando (TR). não só o Trancoso da Silva -> Para respoder pergunta 4.
--- 5?
-
---------------------------------------------------------------------------------------------------
-select hr_controle_qualidade, count(*) --função de agregação
-from producao.controle_qualidade
-group by hr_controle_qualidade
-order by hr_controle_qualidade;
---------------------------------------------------------------------------------------------------
