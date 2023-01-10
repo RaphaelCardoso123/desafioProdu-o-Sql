ddf8c7b5bb2caab83ff657057d39a0d94ab0a5a7
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2023-01-09 21:34:25 -0300

    ...

--- desafioSql.sql
+++ desafioSql.sql
@@ -77,23 +77,23 @@ create table producao.controle_qualidade(
 
 begin transaction;
 insert into producao.linha_producao(dt_linha_producao)
-values ('20221201'), ('20221205'), ('20221207'), ('20221209'), ('20221210') 
-	  ,('20221214'), ('20221216'), ('20221218'), ('20221220'), ('20221222')
+values   ('20221201'), ('20221205'), ('20221207'), ('20221209'), ('20221210') 
+	    ,('20221214'), ('20221216'), ('20221218'), ('20221220'), ('20221222')
 commit;
 select * from producao.linha_producao;
 
 
 begin transaction;
 insert into producao.tipo_produto(nm_tipo_produto)
-values('Geladeira'), ('Máquina de lavar'), ('Fogão'), ('Freezer'), ('Frigobar')
+values   ('Geladeira'), ('Máquina de lavar'), ('Fogão'), ('Freezer'), ('Frigobar')
 commit;
 select * from producao.tipo_produto;
 
 
 begin transaction;
 insert into producao.avaliacao(sg_avaliacao, ds_avaliacao)
-values ('OK', 'Liberado'), ('EL', 'Problema elétrico'), ('PT', 'Problema de pintura')
-	  ,('PE', 'Problema na estrutura'), ('TR', 'Todo rejeitado')
+values   ('OK', 'Liberado'), ('EL', 'Problema elétrico'), ('PT', 'Problema de pintura')
+	    ,('PE', 'Problema na estrutura'), ('TR', 'Todo rejeitado')
 commit;
 select * from producao.avaliacao;
 
@@ -101,31 +101,31 @@ select * from producao.avaliacao;
 --como tirar os zeros dos horários??
 begin transaction;
 insert into producao.inspetor(nm_inspetor, dt_trabalho, hr_inicio_trabalho, hr_fim_trabalho)
-values ('Trancoso da Silva', '20221201', '9:05', '11:05'), ('Trancoso da Silva', '20221205', '9:00', '11:00')
-	  ,('Trancoso da Silva', '20221207', '9:10', '11:10'), ('Trancoso da Silva', '20221216', '9:00', '11:10')
-	  ,('Pedro do Monte', '20221209', '9:00', '11:00'), ('Pedro do Monte', '20221210', '9:10', '11:10')
-	  ,('José Carmelo', '20221214', '9:05', '11:05'), ('José Carmelo', '20221218', '9:15', '11:15')
-	  ,('Júlio Cardoso', '20221220', '9:00', '11:15'), ('Júlio Cardoso', '20221222', '9:00', '11:00')
+values   ('Trancoso da Silva', '20221201', '9:05', '11:05'), ('Trancoso da Silva', '20221205', '9:00', '11:00')
+	    ,('Trancoso da Silva', '20221207', '9:10', '11:10'), ('Trancoso da Silva', '20221216', '9:00', '11:10')
+	    ,('Pedro do Monte', '20221209', '9:00', '11:00'), ('Pedro do Monte', '20221210', '9:10', '11:10')
+	    ,('José Carmelo', '20221214', '9:05', '11:05'), ('José Carmelo', '20221218', '9:15', '11:15')
+	    ,('Júlio Cardoso', '20221220', '9:00', '11:15'), ('Júlio Cardoso', '20221222', '9:00', '11:00')
 commit;
 select * from producao.inspetor;
 
 
 begin transaction; 
 insert into producao.ficha(dt_inspecao, cd_matricula_inspetor, nm_inspetor)
-values ('20221201', 3, 'Trancoso da Silva'), ('20221205', 3, 'Trancoso da Silva')
-	  ,('20221207', 3, 'Trancoso da Silva'), ('20221216', 3, 'Trancoso da Silva') 
-	  ,('20221209', 1, 'Pedro do Monte'), ('20221210', 1, 'Pedro do Monte')
-	  ,('20221214', 2, 'José Carmelo'), ('20221218', 2, 'José Carmelo')
-	  ,('20221220', 4, 'Júlio Cardoso'), ('20221222', 4, 'Júlio Cardoso')
-	  ,('20221224', 2, 'José Carmelo'), ('20221225', 2, 'José Carmelo')
-	  ,('20221226', 4, 'Júlio Cardoso'), ('20221227', 4, 'Júlio Cardoso'), ('20221228', 4, 'Júlio Cardoso')
+values   ('20221201', 3, 'Trancoso da Silva'), ('20221205', 3, 'Trancoso da Silva')
+	    ,('20221207', 3, 'Trancoso da Silva'), ('20221216', 3, 'Trancoso da Silva') 
+	    ,('20221209', 1, 'Pedro do Monte'), ('20221210', 1, 'Pedro do Monte')
+	    ,('20221214', 2, 'José Carmelo'), ('20221218', 2, 'José Carmelo')
+	    ,('20221220', 4, 'Júlio Cardoso'), ('20221222', 4, 'Júlio Cardoso')
+	    ,('20221224', 2, 'José Carmelo'), ('20221225', 2, 'José Carmelo')
+	    ,('20221226', 4, 'Júlio Cardoso'), ('20221227', 4, 'Júlio Cardoso'), ('20221228', 4, 'Júlio Cardoso')
 commit;
 select * from producao.ficha;
 
 begin transaction;
 insert into producao.produto(dt_produto, cd_linha_producao, cd_tipo_produto, nm_tipo_produto)
-values ('20221201', 1, 2, 'Máquina de lavar'), ('20221209', 2, 3, 'Fogão'), ('20221216', 1, 1, 'Geladeira')
-	  ,('20221218', 4, 5, 'Frigobar'), ('20221222', 5, 4, 'Freezer')
+values   ('20221201', 1, 2, 'Máquina de lavar'), ('20221209', 2, 3, 'Fogão'), ('20221216', 1, 1, 'Geladeira')
+	    ,('20221218', 4, 5, 'Frigobar'), ('20221222', 5, 4, 'Freezer')
 commit;
 select * from producao.produto;
 
@@ -133,15 +133,15 @@ select * from producao.produto;
 --não pode ser identity (1, 1) auto incrementavel pq é um código único para cada inspetor.
 begin transaction; 
 insert into producao.controle_qualidade	
-	 (dt_controle_qualidade,		hr_inicio_controle_qualidade,	hr_fim_controle_qualidade,	 cd_numero_ficha
-	 ,dt_inspecao,	cd_matricula_inspetor,	nm_inspetor, cd_id_produto,		cd_linha_producao,	 dt_linha_producao
-	 ,cd_tipo_produto,   	sg_avaliacao)
-values('20221201', '8:00', '11:55', 1, '20221201', 1, 'Trancoso da Silva', 1, 1, '20221201', 2, 'TR'), 
-	  ('20221205', '8:00', '11:55', 1, '20221205', 2, 'Trancoso da Silva', 5, 2, '20221205', 3, 'TR'),
-	  ('20221207', '8:00', '11:55', 1, '20221207', 3, 'Trancoso da Silva',  3, 3, '20221207', 3, 'OK'),
-	  ('20221216', '8:00', '11:55', 1, '20221216', 4, 'Trancoso da Silva',  1, 7, '20221216', 1, 'TR'), 
-	  ('20221209', '8:00', '11:55', 2, '20221209', 6, 'Pedro do Monte', 2, 4, '20221209', 5, 'EL'),
-	  ('20221220', '8:00', '11:55', 3, '20221220', 9, 'Júlio Cardoso', 4, 9, '20221220', 4, 'PE')
+	     (dt_controle_qualidade,		hr_inicio_controle_qualidade,	hr_fim_controle_qualidade,	 cd_numero_ficha
+	     ,dt_inspecao,	cd_matricula_inspetor,	nm_inspetor, cd_id_produto,		cd_linha_producao,	 dt_linha_producao
+	     ,cd_tipo_produto,   	sg_avaliacao)
+values   ('20221201', '8:00', '11:55', 1, '20221201', 1, 'Trancoso da Silva', 1, 1, '20221201', 2, 'TR')
+	    ,('20221205', '8:00', '11:55', 1, '20221205', 2, 'Trancoso da Silva', 5, 2, '20221205', 3, 'TR')
+	    ,('20221207', '8:00', '11:55', 1, '20221207', 3, 'Trancoso da Silva',  3, 3, '20221207', 3, 'OK')
+	    ,('20221216', '8:00', '11:55', 1, '20221216', 4, 'Trancoso da Silva',  1, 7, '20221216', 1, 'TR') 
+	    ,('20221209', '8:00', '11:55', 2, '20221209', 6, 'Pedro do Monte', 2, 4, '20221209', 5, 'EL')
+	    ,('20221220', '8:00', '11:55', 3, '20221220', 9, 'Júlio Cardoso', 4, 9, '20221220', 4, 'PE')
 commit;
 select * from producao.controle_qualidade;
 
